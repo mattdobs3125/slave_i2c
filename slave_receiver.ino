47ca4f12a9da677d8cf66d031ea22de9fa743ebc
mattdobs3125 <matthew.dobrowolski5@gmail.com>
2018-10-24 02:15:21 -0600

    first

---
+++ slave_receiver.ino
@@ -0,0 +1,62 @@
+// Wire Slave Receiver
+// by Nicholas Zambetti <http://www.zambetti.com>
+
+// Demonstrates use of the Wire library
+// Receives data as an I2C/TWI slave device
+// Refer to the "Wire Master Writer" example for use with this
+
+// Created 29 March 2006
+
+// This example code is in the public domain.
+
+const int trigPin = 9;
+const int echoPin = 11;
+#include <Wire.h>
+long duration;
+int distance;
+
+void setup() {
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); 
+  Serial.begin(9600);           // start serial for output
+}
+
+void loop() {
+  delay(100);
+ 
+//  Serial.print("we are hnere1");
+//  delay(1000);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  while (1 < Wire.available()) { // loop through all but the last
+//    Serial.print("we are hnere2");
+//    delay(1000);
+    char c = Wire.read();
+    Serial.print(c);
+   if (c=="c")
+   {
+    Serial.print("we 32323");
+     digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+   }
+            // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
+}
